5a72a430da54a8c56506fa0e0b6b2342ba70d243
Tuugii <85017432+Tuugiixtyl@users.noreply.github.com>
2023-12-14 10:41:24 +0800

    Add project files.

---
+++ xoxo/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace xoxo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            contextMenuStrip2 = new ContextMenuStrip(components);
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripMenuItem();
+            toolStripMenuItem3 = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripMenuItem();
+            a1 = new Button();
+            a2 = new Button();
+            a3 = new Button();
+            b1 = new Button();
+            b2 = new Button();
+            b3 = new Button();
+            c1 = new Button();
+            c2 = new Button();
+            c3 = new Button();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            contextMenuStrip2.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip2.Name = "contextMenuStrip2";
+            contextMenuStrip2.Size = new Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(259, 28);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem2, toolStripMenuItem3 });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(123, 26);
+            toolStripMenuItem2.Text = "Start";
+            // 
+            // toolStripMenuItem3
+            // 
+            toolStripMenuItem3.Name = "toolStripMenuItem3";
+            toolStripMenuItem3.Size = new Size(123, 26);
+            toolStripMenuItem3.Text = "Exit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripMenuItem1 });
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(55, 24);
+            helpToolStripMenuItem.Text = "Help";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(133, 26);
+            toolStripMenuItem1.Text = "About";
+            // 
+            // a1
+            // 
+            a1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            a1.Location = new Point(12, 31);
+            a1.Name = "a1";
+            a1.Size = new Size(75, 75);
+            a1.TabIndex = 3;
+            a1.TabStop = false;
+            a1.UseVisualStyleBackColor = true;
+            a1.Click += a1_Click;
+            // 
+            // a2
+            // 
+            a2.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            a2.Location = new Point(93, 31);
+            a2.Name = "a2";
+            a2.Size = new Size(75, 75);
+            a2.TabIndex = 4;
+            a2.TabStop = false;
+            a2.UseVisualStyleBackColor = true;
+            a2.Click += a2_Click;
+            // 
+            // a3
+            // 
+            a3.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            a3.Location = new Point(174, 31);
+            a3.Name = "a3";
+            a3.Size = new Size(75, 75);
+            a3.TabIndex = 5;
+            a3.TabStop = false;
+            a3.UseVisualStyleBackColor = true;
+            // 
+            // b1
+            // 
+            b1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            b1.Location = new Point(12, 112);
+            b1.Name = "b1";
+            b1.Size = new Size(75, 75);
+            b1.TabIndex = 6;
+            b1.TabStop = false;
+            b1.UseVisualStyleBackColor = true;
+            // 
+            // b2
+            // 
+            b2.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            b2.Location = new Point(93, 112);
+            b2.Name = "b2";
+            b2.Size = new Size(75, 75);
+            b2.TabIndex = 7;
+            b2.TabStop = false;
+            b2.UseVisualStyleBackColor = true;
+            // 
+            // b3
+            // 
+            b3.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            b3.Location = new Point(174, 112);
+            b3.Name = "b3";
+            b3.Size = new Size(75, 75);
+            b3.TabIndex = 8;
+            b3.TabStop = false;
+            b3.UseVisualStyleBackColor = true;
+            // 
+            // c1
+            // 
+            c1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            c1.Location = new Point(12, 193);
+            c1.Name = "c1";
+            c1.Size = new Size(75, 75);
+            c1.TabIndex = 9;
+            c1.TabStop = false;
+            c1.UseVisualStyleBackColor = true;
+            // 
+            // c2
+            // 
+            c2.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            c2.Location = new Point(93, 193);
+            c2.Name = "c2";
+            c2.Size = new Size(75, 75);
+            c2.TabIndex = 10;
+            c2.TabStop = false;
+            c2.UseVisualStyleBackColor = true;
+            // 
+            // c3
+            // 
+            c3.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            c3.Location = new Point(174, 193);
+            c3.Name = "c3";
+            c3.Size = new Size(75, 75);
+            c3.TabIndex = 11;
+            c3.TabStop = false;
+            c3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(259, 275);
+            Controls.Add(c3);
+            Controls.Add(c2);
+            Controls.Add(c1);
+            Controls.Add(b3);
+            Controls.Add(b2);
+            Controls.Add(b1);
+            Controls.Add(a3);
+            Controls.Add(a2);
+            Controls.Add(a1);
+            Controls.Add(menuStrip1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            MaximizeBox = false;
+            MaximumSize = new Size(277, 322);
+            MinimumSize = new Size(277, 322);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "XoXo тоглоом ";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ContextMenuStrip contextMenuStrip1;
+        private ContextMenuStrip contextMenuStrip2;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem2;
+        private ToolStripMenuItem toolStripMenuItem3;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private Button a1;
+        private Button a2;
+        private Button a3;
+        private Button b1;
+        private Button b2;
+        private Button b3;
+        private Button c1;
+        private Button c2;
+        private Button c3;
+    }
+}
---
+++ xoxo/Form1.cs
@@ -0,0 +1,402 @@
+﻿namespace xoxo
+{
+    public partial class Form1 : Form
+    {
+        int player = 1, click = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            WireUpButtons();
+        }
+
+        void WireUpButtons()
+        {
+            foreach (var btns in this.Controls.OfType<Button>())
+            {
+                btns.Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = sender as Button;
+
+                if (btn != null && btn.Text == "")
+                {
+                    btn.Text = (player == 1) ? "X" : "O";
+                    player = (player == 1) ? 2 : 1;
+                    btn.Enabled = false;
+
+                    click++;
+                    isWon();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        void restart()
+        {
+            foreach (var btns in this.Controls.OfType<Button>())
+            {
+                btns.Text = "";
+                btns.Enabled = true;
+                click = 0;
+            }
+        }
+
+        void isWon()
+        {
+            bool yalagch = false;
+
+            // Check horizontal wins
+            if ((a1.Text == a2.Text && a2.Text == a3.Text && a1.Enabled == false) ||
+                (b1.Text == b2.Text && b2.Text == b3.Text && b1.Enabled == false) ||
+                (c1.Text == c2.Text && c2.Text == c3.Text && c1.Enabled == false))
+            {
+                yalagch = true;
+            }
+
+            // Check vertical wins
+            if ((a1.Text == b1.Text && b1.Text == c1.Text && a1.Enabled == false) ||
+                (a2.Text == b2.Text && b2.Text == c2.Text && a2.Enabled == false) ||
+                (a3.Text == b3.Text && b3.Text == c3.Text && a3.Enabled == false))
+            {
+                yalagch = true;
+            }
+
+            // Check diagonal wins
+            if ((a1.Text == b2.Text && b2.Text == c3.Text && a1.Enabled == false) ||
+                (a3.Text == b2.Text && b2.Text == c1.Text && a3.Enabled == false))
+            {
+                yalagch = true;
+            }
+
+            if (yalagch)
+            {
+                if (player == 1)
+                    MessageBox.Show("Тоглогч 'O' яллаа!");
+                else
+                    MessageBox.Show("Тоглогч 'X' яллаа!");
+                restart();
+            }
+            else if (click == 9)
+            {
+                MessageBox.Show("Тэнцлээ!");
+                restart();
+            }
+        }
+
+
+        private void a1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+
+        private void a2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void a3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+        
+        private void b1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void b2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void b3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void c1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void c2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void c3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Button btn = (Button)sender;
+
+                if (!(sender is Button))
+                {
+                    MessageBox.Show("Unexpected sender type.");
+                    return;
+                }
+
+                if (player == 1)
+                {
+                    btn.Text = "X";
+                    player = 2;
+                }
+                else
+                {
+                    btn.Text = "O";
+                    player = 1;
+                }
+                btn.Enabled = false;
+
+                click++;
+
+                isWon();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
